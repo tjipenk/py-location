3761c80710b08187c73b7de62a23eb3261bb1d9f
Alvin F <alvin.f@sucofindo.co.id>
2024-06-19 14:26:05 +0700

    add cc_3

--- app/lokasi.py
+++ app/lokasi.py
@@ -21,46 +21,47 @@ def list_lokasi():
     # print(cent)
     return result
 
-def geo_lokasi_ori(lat_src, lon_src, lat_dst, lon_dst, peta_kec=peta_kec):
-    ## source lat lon -6.2515958, 106.8415269
-    # lat_src = -6.2515958
-    # lon_src = 106.8415269
+# def geo_lokasi_ori(lat_src, lon_src, lat_dst, lon_dst, peta_kec=peta_kec):
+#     ## source lat lon -6.2515958, 106.8415269
+#     # lat_src = -6.2515958
+#     # lon_src = 106.8415269
 
-    ## destination lat lon -6.562744, 106.726071
-    # lat_dst = -6.562744
-    # lon_dst = 106.726071
+#     ## destination lat lon -6.562744, 106.726071
+#     # lat_dst = -6.562744
+#     # lon_dst = 106.726071
 
-    # lokasi = gpd.points_from_xy(lon,lat, crs="EPSG:4326") ## WGS84
-    lokasi = pd.DataFrame(
-        {
-            "keterangan" : ['src','dst'],
-            "lat" : [lat_src, lat_dst],
-            "lon" : [lon_src, lon_dst],
-        }
-    )
+#     # lokasi = gpd.points_from_xy(lon,lat, crs="EPSG:4326") ## WGS84
+#     lokasi = pd.DataFrame(
+#         {
+#             "keterangan" : ['src','dst'],
+#             "lat" : [lat_src, lat_dst],
+#             "lon" : [lon_src, lon_dst],
+#         }
+#     )
 
-    lokasi_4326 = gpd.GeoDataFrame(
-        lokasi, geometry=gpd.points_from_xy(lokasi.lon, lokasi.lat), crs="EPSG:4326"
-    )
-    ## EPSG:3857 WGS 84 / Pseudo-Mercator -- Spherical Mercator, Google Maps, OpenStreetMap, Bing, ArcGIS, ESRI
-    lokasi_utm = lokasi_4326.to_crs('EPSG:3857')
+#     lokasi_4326 = gpd.GeoDataFrame(
+#         lokasi, geometry=gpd.points_from_xy(lokasi.lon, lokasi.lat), crs="EPSG:4326"
+#     )
+#     ## EPSG:3857 WGS 84 / Pseudo-Mercator -- Spherical Mercator, Google Maps, OpenStreetMap, Bing, ArcGIS, ESRI
+#     lokasi_utm = lokasi_4326.to_crs('EPSG:3857')
 
-    point_src = lokasi_utm
-    point_dst = point_src.shift() 
+#     point_src = lokasi_utm
+#     point_dst = point_src.shift() 
 
-    jarak = point_src.distance(point_dst)[1]/1000
-    # print("Jarak :", round(jarak,2) ," km")
+#     jarak = point_src.distance(point_dst)[1]/1000
+#     # print("Jarak :", round(jarak,2) ," km")
 
-    points_within = gpd.sjoin(lokasi_4326, peta_kec, how='inner', predicate='within')
-    info_src = points_within
-    #print("Lokasi Asal :",info_src.NAME_1[0], info_src.NAME_2[0], info_src.NAME_3[0] )
-    #print("Lokasi Tujuan :",info_src.NAME_1[1], info_src.NAME_2[1], info_src.NAME_3[1] )
-    result = {
-        "jarak_km" : round(jarak,2),
-        "lokasi_asal" : info_src.NAME_1[0] + ', ' + info_src.NAME_2[0] + ", " + info_src.NAME_3[0] + ", " + info_src.CC_3[0],
-        "lokasi_tujuan" : info_src.NAME_1[1] + ', ' + info_src.NAME_2[1] + ", " + info_src.NAME_3[1] + ", " + info_src.CC_3[1]
-    }
-    return result
+#     points_within = gpd.sjoin(lokasi_4326, peta_kec, how='inner', predicate='within')
+#     info_src = points_within
+#     print(info_src.CC_3[0], info_src.CC_3[1])
+#     #print("Lokasi Asal :",info_src.NAME_1[0], info_src.NAME_2[0], info_src.NAME_3[0] )
+#     #print("Lokasi Tujuan :",info_src.NAME_1[1], info_src.NAME_2[1], info_src.NAME_3[1] )
+#     result = {
+#         "jarak_km" : round(jarak,2),
+#         "lokasi_asal" : info_src.NAME_1[0] + ', ' + info_src.NAME_2[0] + ", " + info_src.NAME_3[0] + ", " + info_src.CC_3[0],
+#         "lokasi_tujuan" : info_src.NAME_1[1] + ', ' + info_src.NAME_2[1] + ", " + info_src.NAME_3[1] + ", " + info_src.CC_3[1]
+#     }
+#     return result
 
 def geo_lokasi(lat_src, lon_src, lat_dst, lon_dst, peta_kec=peta_kec):
     # Construct the OSRM route API request URL
@@ -99,12 +100,16 @@ def geo_lokasi(lat_src, lon_src, lat_dst, lon_dst, peta_kec=peta_kec):
         + ", "
         + info_src.NAME_2[0]
         + ", "
-        + info_src.NAME_3[0],
+        + info_src.NAME_3[0]
+        + ", "
+        + info_src.CC_3[0],
         "lokasi_tujuan": info_src.NAME_1[1]
         + ", "
         + info_src.NAME_2[1]
         + ", "
-        + info_src.NAME_3[1],
+        + info_src.NAME_3[1]
+        + ", "
+        + info_src.CC_3[1],
     }
     return result
 
